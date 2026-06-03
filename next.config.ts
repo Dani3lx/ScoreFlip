e1bcbcdc37eb7e2edde0558948ba1bc50dc9b021
Dani3lx <1danielxu@gmail.com>
2026-06-03 13:26:38 -0400

    chore: deprecated next-pwa

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,7 @@
-import withPWA from "next-pwa";
+import type { NextConfig } from "next";
 
-const nextConfig = withPWA({
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-    runtimeCaching: [
-        {
-            urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i,
-            handler: "CacheFirst",
-            options: {
-                cacheName: "mediapipe-cdn",
-                expiration: { maxEntries: 50, maxAgeSeconds: 60 * 60 * 24 * 30 },
-            },
-        },
-        {
-            urlPattern: /^https:\/\/storage\.googleapis\.com\/.*/i,
-            handler: "CacheFirst",
-            options: {
-                cacheName: "mediapipe-models",
-                expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 30 },
-            },
-        },
-    ],
-})({
-    turbopack: {},
-});
+const nextConfig: NextConfig = {
+    /* config options here */
+};
 
 export default nextConfig;
