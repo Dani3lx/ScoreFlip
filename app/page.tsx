af8dedc670b03c9ef68f25af91ead47d622459cd
Dani3lx <1danielxu@gmail.com>
2026-06-04 14:51:04 -0400

    feat: shadcn components for file upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,31 @@
 "use client";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import ScoreDisplay from "@/features/ScoreDisplay";
+import { FileUpload } from "@/components/reui/fileUpload";
+import { FileWithPreview } from "@/hooks/use-file-upload";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
     const [file, setFile] = useState<string | null>(null);
-
+    const [ready, setReady] = useState<boolean>(false);
+    const uploadFile = useCallback((files: FileWithPreview[]) => {
+        if (!files.length) return;
+        const f = files[0].file;
+        if (f instanceof File) {
+            setFile(URL.createObjectURL(f));
+        }
+    }, []);
     return (
         <>
-            <input
-                type="file"
-                accept=".pdf"
-                onChange={(e) => {
-                    const f = e.target.files?.[0];
-                    if (f) setFile(URL.createObjectURL(f));
-                }}
-            />
-            <ScoreDisplay file={file} />
+            <div className="flex flex-col w-full h-screen items-center justify-center gap-4">
+                {!ready && <FileUpload onFilesChange={uploadFile} />}
+                {file && !ready && (
+                    <Button size="lg" variant={"outline"} onClick={() => setReady(true)}>
+                        Continue
+                    </Button>
+                )}
+                {file && ready && <ScoreDisplay file={file} />}
+            </div>
         </>
     );
 }
