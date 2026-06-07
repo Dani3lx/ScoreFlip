e4f968450be3c0be9688ba6283bf4819e29443d4
Dani3lx <1danielxu@gmail.com>
2026-06-07 12:11:17 -0400

    feat: implemented change file

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useCallback } from "react";
-import ScoreDisplay from "@/features/ScoreDisplay";
+import ScoreDisplay from "@/components/ScoreDisplay";
 import { FileUpload } from "@/components/reui/fileUpload";
 import { FileWithPreview } from "@/hooks/use-file-upload";
 import { Button } from "@/components/ui/button";
@@ -10,21 +10,39 @@ export default function Home() {
     const [ready, setReady] = useState<boolean>(false);
     const uploadFile = useCallback((files: FileWithPreview[]) => {
         if (!files.length) return;
+    
         const f = files[0].file;
-        if (f instanceof File) {
-            setTimeout(() => setFile(URL.createObjectURL(f)), 0);
-        }
+        if (!(f instanceof File)) return;
+    
+        const newUrl = URL.createObjectURL(f);
+    
+        queueMicrotask(() => {
+            setFile((prev) => {
+                if (prev) {
+                    URL.revokeObjectURL(prev);
+                }
+    
+                return newUrl;
+            });
+        });
     }, []);
+
+    const clearFile = useCallback((file: string) => {
+        URL.revokeObjectURL(file);
+        setFile(null);
+        setReady(false);
+    }, []);
+    
     return (
         <>
-            <div className="flex flex-col w-full h-screen items-center justify-center gap-4">
+            <div className="flex flex-col w-full h-screen items-center justify-center gap-4 min-w-100">
                 {!ready && <FileUpload onFilesChange={uploadFile} />}
                 {file && !ready && (
                     <Button size="lg" variant={"outline"} onClick={() => setReady(true)}>
                         Continue
                     </Button>
                 )}
-                {file && ready && <ScoreDisplay file={file} />}
+                {file && ready && <ScoreDisplay file={file} onResetFile={clearFile} />}
             </div>
         </>
     );
---
+++ components/ScoreDisplay/index.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import dynamic from "next/dynamic";
+import { useState, useCallback, useRef } from "react";
+
+const PDFViewer = dynamic(() => import("@/components/PDFViewer"), {
+  ssr: false,
+});
+const BlinkDetector = dynamic(() => import("@/components/BlinkDetector"), {
+  ssr: false,
+});
+
+export default function ScoreDisplay({ file, onResetFile }: { file: string | null, onResetFile: (file: string) => void }) {
+  const [pageNumber, setPageNumber] = useState(1);
+  const [numPages, setNumPages] = useState(0);
+  const numPagesRef = useRef(0);
+
+  const nextPage = useCallback(() => {
+    setPageNumber((p) => Math.min(p + 1, numPagesRef.current));
+  }, []);
+
+  const prevPage = useCallback(() => {
+    setPageNumber((p) => Math.max(p - 1, 1));
+  }, []);
+
+  return (
+    <>
+      {file && (
+        <div className="flex flex-col items-center max-sm:justify-between h-screen">
+          <div className="flex justify-center gap-4 items-center w-screen p-4">
+            <Button variant="secondary" className="absolute left-4" onClick={() => onResetFile(file)}>Change File</Button>
+            <div className="flex gap-4 items-center justify-center">
+              <Button onClick={prevPage}>Back</Button>
+              <span>
+                {pageNumber} / {numPages}
+              </span>
+              <Button onClick={nextPage}>Next</Button>
+            </div>
+
+            <BlinkDetector
+              onDoubleBlink={nextPage}
+              className="hidden sm:block absolute right-4"
+            />
+          </div>
+
+          <PDFViewer
+            file={file}
+            pageNumber={pageNumber}
+            setNumPages={(n) => {
+              numPagesRef.current = n;
+              setNumPages(n);
+            }}
+          />
+
+          <BlinkDetector onDoubleBlink={nextPage} className="sm:hidden p-4" />
+        </div>
+      )}
+    </>
+  );
+}
